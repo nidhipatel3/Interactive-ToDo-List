7771b95991683dfdbbf76f70f2d6fbfc8c2220eb
nidhipatel3 <jbsnidhipatel14@gmail.com>
2025-05-13 13:10:16 -0400

    Install packages and server configuration mongodb connection set routing set view engine set middleware

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const taskRoute = require("./routes/task");
 
 const app = express();
 const PORT = 9001;
+
+mongoose.connect("mongodb://localhost:27017/tododb")
+    .then(() => console.log("MongoDB Connected"));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public/js")));
+app.use(express.static(path.join(__dirname, "public/css")));
+
+app.get('/', (req, res) => {
+    return res.render("home")
+});
+
+app.use("/task", taskRoute);
 
 app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
